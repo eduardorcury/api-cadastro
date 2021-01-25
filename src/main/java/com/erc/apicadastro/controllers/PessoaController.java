dee67c0a2a146fb7021c1b46cdb004ae5d9c86ef
Eduardo Ribeiro <eduardoribeirocury@gmail.com>
2021-01-25 16:18:19 -0300

    Criando controller. Increments #4

---
+++ src/main/java/com/erc/apicadastro/controllers/PessoaController.java
@@ -0,0 +1,58 @@
+package com.erc.apicadastro.controllers;
+
+import com.erc.apicadastro.dto.PessoaDTO;
+import com.erc.apicadastro.mapper.PessoaMapper;
+import com.erc.apicadastro.services.PessoaService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(path = "/api/v1/pessoas")
+public class PessoaController {
+
+    private final PessoaService pessoaService;
+    private final PessoaMapper mapper = PessoaMapper.INSTANCE;
+
+    public PessoaController(PessoaService pessoaService) {
+        this.pessoaService = pessoaService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PessoaDTO>> listarTodos() {
+        List<PessoaDTO> pessoas = pessoaService.encontrarTodos()
+                .stream().map(mapper::toDTO)
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(pessoas, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PessoaDTO> encontrarPorId(@PathVariable(value = "id") Integer pessoaId) {
+        PessoaDTO pessoa = mapper.toDTO(pessoaService.encontrarPorId(pessoaId));
+        //TODO checar id errado
+        return new ResponseEntity<>(pessoa, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> salvar(@RequestBody @Valid PessoaDTO pessoaDTO) {
+        pessoaService.salvar(mapper.toDomain(pessoaDTO));
+        return new ResponseEntity<>("Pessoa cadastrada com sucesso", HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> atualizar(@PathVariable(value = "id") Integer pessoaId, @Valid PessoaDTO pessoaDTO) {
+        pessoaService.atualizar(pessoaId, pessoaDTO);
+        return new ResponseEntity<>("Pessoa atualizada com sucesso", HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletar(@PathVariable(value = "id") Integer pessoaId) {
+        pessoaService.deletar(pessoaId);
+    }
+
+}
