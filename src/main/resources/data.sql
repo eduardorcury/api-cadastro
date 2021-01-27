50b0854aedfe80a7fb4a6c986b8241f6f48f9bf6
Eduardo Ribeiro Cury <60987061+eduardorcury@users.noreply.github.com>
2021-01-27 17:10:34 -0300

    Prepopulando banco de dados

---
+++ src/main/resources/data.sql
@@ -0,0 +1,22 @@
+INSERT INTO pessoa (nome, cpf, nascimento)
+VALUES ('Eduardo', '924.952.340-89', '1998-02-03'),
+	('Jõao', '697.682.350-63', '1990-12-05'),
+	('Marcela', '194.358.600-44', '1967-06-20'),
+	('Ana', '000.652.860-06', '1998-07-10'),
+	('Rafael', '968.661.920-80', '1996-06-01'),
+	('Carlos', '238.638.560-40', '2006-07-10'),
+	('Eduarda', '398.320.420-11', '1999-10-19');
+
+
+INSERT INTO contato (nome, telefone, email, pessoa_id)
+VALUES ('Daniel', '989898', 'daniel@gmail.com', 1),
+	('Maria', '123412', 'maria@gmail.com', 1),
+	('Tânia', '67868', 'tania@gmail.com', 2),
+	('Felipe', '123213', 'felipe@gmail.com', 2),
+	('Carlos', '678678', 'carlos@gmail.com', 2),
+	('Rafaela', '80890', 'rafaela@gmail.com', 3),
+	('Luiza', '324453', 'luiza@gmail.com', 4),
+	('Clarinda', '567567', 'clarinda@gmail.com', 5),
+	('Débora', '345456', 'debora@gmail.com', 5),
+	('Victor', '687686', 'victor@gmail.com', 6),
+	('Roberto', '98934535', 'roberto@gmail.com', 7);
